7c1e8a95c57961f5a00a98d889bca1eaa3a7ca1d
Tiago Gomes <contato@tiagogomes.dev.br>
2024-11-11 22:05:33 -0300

    Game seed

--- src/main/java/br/dev/tiagogomes/dslist/entities/Game.java
+++ src/main/java/br/dev/tiagogomes/dslist/entities/Game.java
@@ -22,6 +22,8 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	@Column (columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column (columnDefinition = "TEXT")
 	private String longDescription;
 }
